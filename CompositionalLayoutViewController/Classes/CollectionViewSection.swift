3cfbd922126c5e996634f2fb97b6b3b280fd7a36
Akira <akira.matsuda@me.com>
2022-01-04 16:02:04 +0900

    Add nonce to make section unique.

--- CompositionalLayoutViewController/Classes/CollectionViewSection.swift
+++ CompositionalLayoutViewController/Classes/CollectionViewSection.swift
@@ -16,14 +16,46 @@ public protocol CollectionViewSection {
     func cell(_ collectionView: UICollectionView, indexPath: IndexPath) -> UICollectionViewCell?
     func supplementaryView(_ collectionView: UICollectionView, kind: String, indexPath: IndexPath) -> UICollectionReusableView?
     func configureSupplementaryView(_ view: UICollectionReusableView, indexPath: IndexPath)
+    
+    mutating func makeUnique()
 }
 
-extension CollectionViewSection {
+public extension CollectionViewSection {
+    var nonce: String? {
+        get {
+            var nonceKey: UInt8 = 0
+            guard let associatedObject = objc_getAssociatedObject(
+                self,
+                &nonceKey
+            ) as? String else {
+                return nil
+            }
+            return associatedObject
+        }
+
+        set(nonce) {
+            var nonceKey: UInt8 = 0
+            objc_setAssociatedObject(
+                self,
+                &nonceKey,
+                nonce,
+                .OBJC_ASSOCIATION_RETAIN
+            )
+        }
+    }
+
     var snapshotSection: AnyHashable {
         var hasher = Hasher()
         snapshotItems.forEach {
             hasher.combine($0)
         }
+        if let nonce = nonce {
+            hasher.combine(nonce)
+        }
         return hasher.finalize()
+    }
+    
+    mutating func makeUnique() {
+        nonce = UUID().uuidString
     }
 }
